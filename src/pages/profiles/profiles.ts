9daa9e92d6447fa22da92774c2b094f85dc7d10b
Joseph Mtinangi <josephmtinangi@gmail.com>
2017-03-14 21:33:52 +0300

    Define variables and import the github service provider

--- src/pages/profiles/profiles.ts
+++ src/pages/profiles/profiles.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
+import { GithubService } from '../../providers/github-service';
+
 /*
   Generated class for the Profiles page.
 
@@ -13,10 +15,18 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class ProfilesPage {
 
+  profiles: any;
+  repos: any;
+  github_user = "";
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {}
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ProfilesPage');
+  }
+
+  onSubmit() {
+
   }
 
 }
